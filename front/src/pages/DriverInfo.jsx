b2399523c356245432a21946a84103a7cee2fd5f
skl1menko <alklimenko227@gmail.com>
2025-05-14 01:08:05 +0300

    refactor: update modal styles and enhance driver assignment display

--- front/src/pages/DriverInfo.jsx
+++ front/src/pages/DriverInfo.jsx
@@ -162,11 +162,13 @@ const DriverInfo = () => {
 
             {showAssignmentsModal && assignments && assignments.length > 0 && (
                 <div className="modal-overlay">
-                    <div className="modal-content">
+                    <div className="modal-content-dr">
                         <h2>Призначення водія</h2>
                         <ul>
                             {assignments.map((assignment, index) => (
-                                <li key={index}>{`Маршрут: ${assignment.scheduleId}, Час відправлення: ${assignment.departureTime}`}</li>
+                                <li key={index}>
+                                    {`Маршрут: ${assignment.scheduleId}, Час відправлення: ${assignment.firstDepartureTime} — ${assignment.lastDepartureTime}`}
+                                </li>
                             ))}
                         </ul>
                         <div className="modal-buttons">
@@ -175,6 +177,7 @@ const DriverInfo = () => {
                     </div>
                 </div>
             )}
+
 
         </div>
     );
